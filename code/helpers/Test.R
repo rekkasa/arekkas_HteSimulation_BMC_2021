786cc5ff039e7047dc8614f029e32a82580be22c
rekkasa <alexandros.rekkas@gmail.com>
2021-10-13 05:45:55 +0200

    plot probabilities with and without treatment

--- code/helpers/Test.R
+++ code/helpers/Test.R
@@ -47,6 +47,7 @@ calcAbsoluteBenefitGomp <- function(p, b) {
   return(benefit)
 }
 
+
 plotAbsoluteBenefitGomp <- function(b, label) {
   ggplot2::stat_function(
     data = data.frame(x = c(.05, .95)),
@@ -143,3 +144,94 @@ ggplot() +
     legend.text = element_text(size = 20),
     legend.title = element_blank()
   ) 
+
+
+
+calcGompertz <- function(x, a, b, c = 0) {
+  
+  ret <- exp(-(a + b * (x - c)))
+  ret <- exp(-ret)
+  
+  return(ret)
+}
+
+
+
+plotRelativeConstant <- function(gamma, label = "constant") {
+  logit <- function(x) log(x / (1 - x))
+  prob <- function(p, gamma) {
+    (exp(logit(p) + gamma)) / (1 + exp(logit(p) + gamma))
+  }
+ 
+  ggplot2::stat_function(
+    data = data.frame(x = c(0, .95)),
+    ggplot2::aes(x = x, color = label),
+    fun = prob,
+    args = list(gamma = gamma)
+  )
+   
+}
+
+
+plotRelativeBenefitGomp <- function(b, harm = .01, label) {
+  
+  ggplot2::stat_function(
+    data = data.frame(x = c(0, .95)),
+    ggplot2::aes(x = x, color = label),
+    fun = function(x, b, harm) x^exp(-b) + harm,
+    args = list(b = b, harm = harm)
+  )
+  
+}
+
+
+plotRelativeRiskOld <- function(gammas, label) {
+  
+  g0 <- gammas$g0
+  g1 <- gammas$g1
+  g2 <- gammas$g2
+  c  <- gammas$c
+  
+  calcRiskTreatment <- function(p, g0, g1, g2, c) {
+   logit <- function(p) log(p / (1 - p))
+   f <- function(x, g0, g1, g2, c) {
+     g0 + g1 * (x - c) + g2 * (x - c)**2
+   }
+    u <- exp(f(x = logit(p), g0 = g0, g1 = g1, g2 = g2, c = c)) 
+    u / (1 + u)
+  }
+  ggplot2::stat_function(
+    data = data.frame(x = c(0, .95)),
+    ggplot2::aes(x = x, color = label),
+    fun = calcRiskTreatment,
+    args = list(
+      g0 = g0,
+      g1 = g1,
+      g2 = g2,
+      c  = c
+    )
+  )
+  
+}
+
+scenarios <- c(46, 55)
+gammas <- list()
+for (i in seq_along(scenarios)) {
+  idSettings <- analysisIds %>%
+    filter(scenario == scenarios[i])
+  gammas[[i]] <- list(
+    g0 = idSettings$g0,
+    g1 = idSettings$g1,
+    g2 = idSettings$g2,
+    c  = idSettings$c
+  )
+}
+
+ggplot() +
+  plotRelativeConstant(0, "absent") +
+  plotRelativeConstant(log(.5)) +
+  plotRelativeBenefitGomp(b = -.1, harm = .01, label = "gompertz") +
+  plotRelativeRiskOld(gammas = gammas[[1]], "moderate") +
+  plotRelativeRiskOld(gammas = gammas[[2]], "strong") +
+  xlim(0, .5)
+e
