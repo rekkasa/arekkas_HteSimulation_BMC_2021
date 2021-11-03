6db8b19761817c9edf6efcb563b8598afdc5961d
rekkasa <a.rekkas@erasmusmc.nl>
2021-11-03 13:57:28 +0100

    setup figure creation script for manuscript plots

--- code/PlotRmseBaseNew.R
+++ code/PlotRmseBaseNew.R
@@ -6,6 +6,7 @@
 
 library(tidyverse)
 library(glue)
+library(ggtext)
 
 source("code/helpers/PlotGammas.R")                # Used to generate the absolute plots
 source("code/helpers/PlotDeviationsFunctions.R")   # Generates the absolute plot
@@ -39,7 +40,7 @@ names(titles) <- NULL
 
 titlePrefix <- paste0(
   "**",
-  LETTERS[1:4],
+  LETTERS[1:5],
   ".**"
 )
 
@@ -71,7 +72,11 @@ processed <- readr::read_csv(
   )
 
 
-plotAbsoluteBenefit <- function(data, label) {
+plotAbsoluteBenefit <- function(data) {
+  calcSquare <- function(x, g0 = 0, g1 = 0, g2 = 0, l = 0) {
+    ret <- g0 + g1 * (x - l) + g2 * (x - l)**2
+    return(ret)
+  }
   calcAbsoluteBenefit <- function(p, g0 = 0, g1 = 0, g2 = 0, l = 0, harm) {
     x <- log(p / (1 - p))
     sq <- calcSquare(x, g0, g1, g2, l)
@@ -82,10 +87,10 @@ plotAbsoluteBenefit <- function(data, label) {
   res <- ggplot()
   for (i in seq_along(harmSettings)) {
     harm <- case_when(
-      data$harm == "moderate-positive" ~ data$averageTrueBenefit / 4, 
-      data$harm == "strong-positive" ~ data$averageTrueBenefit / 4, 
-      data$harm == "negative" ~ -data$averageTrueBenefit / 4, 
-      TRUE                          ~ 0
+      harmSettings[i] == "moderate-positive" ~ data$averageTrueBenefit[1] / 4, 
+      harmSettings[i] == "strong-positive"   ~ data$averageTrueBenefit[1] / 2, 
+      harmSettings[i] == "negative"          ~ -data$averageTrueBenefit[1] / 4, 
+      TRUE                                   ~ 0
     )
     args <- list(
       g0 = data$g0,
@@ -94,18 +99,19 @@ plotAbsoluteBenefit <- function(data, label) {
       l = data$c,
       harm = harm
     )
+    label <- harmSettings[i]
     res <- res + ggplot2::stat_function(
-      data = data.frame(x = c(.05, .95)),
-      ggplot2::aes(x = x, color = label),
+      data = data.frame(x = c(.05, .95), label = label),
+      ggplot2::aes(x = x, color = label, group = label),
       fun = calcAbsoluteBenefit,
       args = args
     )
-    return(res)
   }
-  
+  return(res)
 }
 
 absolutePlots <- scenarioIds %>%
+  filter(harm != "negative") %>%
   group_by(type) %>%
   nest() %>%
   mutate(
@@ -113,12 +119,14 @@ absolutePlots <- scenarioIds %>%
       data,
       type,
       ~ plotAbsoluteBenefit(
-        data = .x,
-        label = .y
+        data = .x
       )
     )
   )
 
+# --------------------
+# Create the boxplots
+# --------------------
 scenarios <- scenarioIds %>%
   filter(harm == "absent") %>%
   select(scenario) %>%
@@ -128,22 +136,143 @@ names(scenarios) <- NULL
 
 plotList <- plotResult(scenarios, processed, titles, metric = metric)
 
-absolutePlot <- generateAbsolutePlot(
-  analysisIds = scenarioIds,
-  scenarios = scenarios,
-  legend.position = "top",
-  legend.title = element_blank(),
-  legend.text = element_text(size = 14),
-  axis.title = element_text(size = 12)
+
+# ----------------------
+# Put them all together
+# ----------------------
+gridList <- list(
+  plotList[[1]] + theme(axis.text.x = element_blank(), legend.position = c(.14, .87)),
+  absolutePlots$plot[[1]] +
+    ggtitle("True absolute benefit in treatment arm") +
+    xlim(c(0, .5)) +
+    ylim(c(-.05, .15)) +
+    scale_color_manual(
+      name = "Constant treatment-\n related harm",
+      values = c(
+        "#26547C",
+        "#06D6A0",
+        "#EF476F"
+      )
+    ) +
+    theme_bw() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.x = element_blank(),
+      plot.title = element_markdown(),
+      legend.position = "none"
+    ),
+  plotList[[2]] + theme(axis.text.x = element_blank(), legend.position = "none"),
+  absolutePlots$plot[[2]] +
+    ggtitle("True absolute benefit in treatment arm") +
+    xlim(c(0, .5)) +
+    ylim(c(-.05, .15)) +
+    scale_color_manual(
+      name = "Constant treatment-\n related harm",
+      values = c(
+        "#26547C",
+        "#06D6A0",
+        "#EF476F"
+      )
+    ) +
+    theme_bw() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.x = element_blank(),
+      plot.title = element_markdown(),
+      legend.position = "none"
+    ),
+  plotList[[3]] + theme(axis.text.x = element_blank(), legend.position = "none"),
+  absolutePlots$plot[[3]] +
+    ggtitle("True absolute benefit in treatment arm") +
+    xlim(c(0, .5)) +
+    ylim(c(-.05, .15)) +
+    scale_color_manual(
+      name = "Constant treatment-\n related harm",
+      values = c(
+        "#26547C",
+        "#06D6A0",
+        "#EF476F"
+      )
+    ) +
+    theme_bw() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.x = element_blank(),
+      plot.title = element_markdown(),
+      legend.position = "none"
+    ),
+  plotList[[4]] + theme(axis.text.x = element_blank(), legend.position = "none"),
+  absolutePlots$plot[[4]] +
+    ggtitle("True absolute benefit in treatment arm") +
+    xlim(c(0, .5)) +
+    ylim(c(-.05, .15)) +
+    scale_color_manual(
+      name = "Constant treatment-\n related harm",
+      values = c(
+        "#26547C",
+        "#06D6A0",
+        "#EF476F"
+      )
+    ) +
+    theme_bw() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      axis.text.x = element_blank(),
+      plot.title = element_markdown(),
+      legend.position = "none"
+    ),
+  plotList[[5]] + theme(legend.position = "none"),
+  absolutePlots$plot[[5]] +
+    ggtitle("True absolute benefit in treatment arm") +
+    xlim(c(0, .5)) +
+    ylim(c(-.05, .15)) +
+    scale_color_manual(
+      name = "Constant treatment-\n related harm",
+      values = c(
+        "#26547C",
+        "#06D6A0",
+        "#EF476F"
+      )
+    ) +
+    theme_bw() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      plot.title = element_markdown(),
+      legend.position = "none"
+    )
+)
+
+cowplot::plot_grid(
+  gridList[[1]],
+  gridList[[2]],
+  gridList[[3]],
+  gridList[[4]],
+  gridList[[5]],
+  gridList[[6]],
+  gridList[[7]],
+  gridList[[8]],
+  gridList[[9]],
+  gridList[[10]],
+  ncol = 2,
+  rel_widths = c(1, .5)
 )
 
 pp <- gridExtra::grid.arrange(
-  absolutePlot,
   plotList[[1]] + theme(axis.text.x = element_blank()),
   plotList[[2]] + theme(legend.position = "none", axis.text.x = element_blank()),
   plotList[[3]] + theme(legend.position = "none", axis.text.x = element_blank()),
   plotList[[4]] + theme(legend.position = "none"),
   plotList[[5]] + theme(legend.position = "none"),
+  absolutePlots$plot[[1]],
+  absolutePlots$plot[[2]],
+  absolutePlots$plot[[3]],
+  absolutePlots$plot[[4]],
+  absolutePlots$plot[[5]],
   ncol = 2,
   left = grid::textGrob(
     expression(
